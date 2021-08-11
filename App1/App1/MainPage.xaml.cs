e7cc044b1e1bdd08f32b0336b52eebc7d2ff8d8a
alex <saa2410@mail.ru>
2021-08-11 17:57:35 +1000

    Add SKiasharp for PDF create

--- App1/App1/MainPage.xaml.cs
+++ App1/App1/MainPage.xaml.cs
@@ -11,6 +11,8 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using App1.Models;
 
+using SkiaSharp;
+
 namespace App1
 {
     public partial class MainPage : ContentPage
@@ -31,6 +33,57 @@ namespace App1
             CheckPackage parameters = (CheckPackage)serialize.Deserialize(stream);
             this.BindingContext = this;
            // MyListView.ItemsSource = parameters.Parameters.CashierINN; //ПОКА ЧТО НЕ РАБОТАЕТ
+        }
+		private bool isSupported = true;
+		private void  Create_PDF(string pathh)
+        {
+            SKDocument document = SKDocument.CreatePdf("hghg");
+			if (document == null)
+			{
+				isSupported = false;
+				return;
+			}
+
+			SKPaint paint = new SKPaint
+			{
+				TextSize = 64.0f,
+				IsAntialias = true,
+				Color = 0xFF9CAFB7,
+				IsStroke = true,
+				StrokeWidth = 3,
+				TextAlign = SKTextAlign.Center
+			};
+
+			var pageWidth = 840;
+			var pageHeight = 1188;
+
+			// draw page 1
+			using (var pdfCanvas = document.BeginPage(pageWidth, pageHeight))
+			{
+				// draw button
+				SKPaint nextPagePaint = new SKPaint
+				{
+					IsAntialias = true,
+					TextSize = 16,
+					Color = SKColors.OrangeRed
+				};
+				var nextText = "Next Page >>";
+				var btn = new SKRect(pageWidth - nextPagePaint.MeasureText(nextText) - 24, 0, pageWidth, nextPagePaint.TextSize + 24);
+				pdfCanvas.DrawText(nextText, btn.Left + 12, btn.Bottom - 12, nextPagePaint);
+				// make button link
+				pdfCanvas.DrawLinkDestinationAnnotation(btn, "next-page");
+
+				// draw contents
+				pdfCanvas.DrawText("...PDF 1/2...", pageWidth / 2, pageHeight / 4, paint);
+				document.EndPage();
+			}
+
+
+		}
+
+		private  void Button_Clicked(object sender, EventArgs e)
+        {
+			Create_PDF("gg");
         }
     }
 }
