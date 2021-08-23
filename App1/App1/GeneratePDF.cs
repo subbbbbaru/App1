d5ccc53e21afb7e6833fb064e0c9f109fe63bf6a
alex <saa2410@mail.ru>
2021-08-23 17:49:15 +1000

    Repair Project for Android internationaliz

--- App1/App1/GeneratePDF.cs
+++ App1/App1/GeneratePDF.cs
@@ -15,11 +15,10 @@ using System.IO;
 using System.Xml.Serialization;
 using System.Reflection;
 using App1.Models;
-using Xamarin.Forms;
 
-[assembly: ExportFont("InputSerif-Thin.ttf", Alias = "InputSerif")]
 namespace App1
 {
+
     public class GeneratePDF
     {
         public void CreatePDFfile(PdfDocument pdfDoc)
@@ -34,12 +33,12 @@ namespace App1
             // var fisk = parameters.Positions.FiscalString.;
 
             //размер страницы PDF документа
-            PageSize pageSize = new PageSize(PageSize.B7);
+            PageSize pageSize = new PageSize(PageSize.A4);
             Document document = new Document(pdfDoc, pageSize);
 
+           
 
-            Stream stream_font = assembly.GetManifestResourceStream("App1.MyResources.InputSerif-Thin1.ttf");
-
+            Stream stream_font = assembly.GetManifestResourceStream("App1.MyResources.Serif-Thin.ttf");
 
 
             var memoryStream = new MemoryStream();
@@ -48,39 +47,44 @@ namespace App1
 
             // https://coderoad.ru/1080442/Как-преобразовать-поток-в-byte-в-C
             // https://docs.microsoft.com/ru-ru/xamarin/xamarin-forms/data-cloud/data/files?tabs=macos
+
+
+            // PdfFont pdfFont = PdfFontFactory.CreateFont(font_Serif, "Cp1251", PdfFontFactory.EmbeddingStrategy.PREFER_EMBEDDED)       
+
             PdfFont pdfFont = PdfFontFactory.CreateFont(font_Serif, PdfEncodings.IDENTITY_H, PdfFontFactory.EmbeddingStrategy.PREFER_EMBEDDED);
 
 
 
 
+            string text1 = "Misha is Vine magnat!";
+            Text text = new Text(text1);
 
-            Text text = new Text("мамам");
             text.SetFont(pdfFont);
             text.SetFontSize(14);
             Paragraph paragraph1 = new Paragraph();
 
-            iText.Kernel.Colors.Color color = new DeviceRgb(255, 100, 20);
-            text.SetFontColor(color);
-
-            //foreach (var fiscalString in parameters.Positions.FiscalString)
-            //{
-            //    paragraph1.Add(fiscalString.Name); 
-            //}
-
-
-
-            paragraph1.Add(text);
-            document.Add(paragraph1);
-
-            
-
+            iText.Kernel.Colors.Color color = new DeviceRgb(255, 150, 20);
+            //text.SetFontColor(color);
+            paragraph1.SetFont(pdfFont);
             //разделитель
             LineSeparator lineSeparator = new LineSeparator(new SolidLine());
             document.Add(lineSeparator);
 
+            foreach (var fiscalString in parameters.Positions.FiscalString)
+            {
+                paragraph1.Add(fiscalString.Name + "________" + fiscalString.PriceWithDiscount);
+                paragraph1.Add("\n");
+            }
 
-            //добавить QR-код в приложение
-            document.Add(CreateQRcode(pdfDoc));
+            paragraph1.Add(text);
+
+            document.SetTextAlignment(TextAlignment.CENTER).Add(paragraph1);
+
+
+            //добавить QR-код в приложение и установить местоположение
+            Paragraph paragraph2 = new Paragraph();
+            paragraph2.SetTextAlignment(TextAlignment.CENTER).Add(CreateQRcode(pdfDoc, text1));
+            document.Add(paragraph2);
 
 
             //сохранение документа
@@ -89,17 +93,21 @@ namespace App1
         }
 
 
-        iText.Layout.Element.Image CreateQRcode(PdfDocument pdfDoc, string time = "1", string sum = "1", string fn = "1", string i = "1", string fp = "1", string n = "1")
+        iText.Layout.Element.Image CreateQRcode(PdfDocument pdfDoc, string text)
         {
             //создание QR-кода
-            BarcodeQRCode barcodeQRCode = new BarcodeQRCode(time + sum + fn + i + fp + n);
+            BarcodeQRCode barcodeQRCode = new BarcodeQRCode(text);
             //конвертация QR-кода в изображение
             var imgBar = new iText.Layout.Element.Image(barcodeQRCode.CreateFormXObject(pdfDoc));
             imgBar.SetWidth(56.6929f);
             imgBar.SetHeight(56.6929f);
-
+            //imgBar.SetTextAlignment(TextAlignment.RIGHT);
             return  imgBar;
         }
+        //private void AddTitle(Document document, string text, int align, )
+        //{
+
+        //}
 
 
     }
